e9913278e20d7178a2ed8985016bef1db3d668e0
pstudio.tk <pstudio.tk@baa2093d-e66a-8f14-2796-971615c58e92>
2010-11-04 16:09:37 +0000

    uploaded base Prototype 01 project.

---
+++ trunk/Prototype 01/Assets/Scripts/Path/scrControlPoint.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+
+public class scrControlPoint : MonoBehaviour {
+
+    scrPath.Parts part;
+    int index;
+
+    public int Index { get { return index; } set { index = (value < 0) ? 0 : value ; } }
+    public scrPath.Parts Part { get { return part; } set { part = value; } }
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	// Gizmo
+	void OnDrawGizmos() {
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, 0.1f);
+	}
+}
