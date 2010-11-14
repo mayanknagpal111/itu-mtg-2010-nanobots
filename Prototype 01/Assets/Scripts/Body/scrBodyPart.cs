f872a66b97f19f1d0a11405a0ecf94a5afb271a9
pstudio.tk <pstudio.tk@baa2093d-e66a-8f14-2796-971615c58e92>
2010-11-14 20:08:17 +0000

    prototype 2 basis for body system added

---
+++ Prototype 01/Assets/Scripts/Body/scrBodyPart.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class scrBodyPart : MonoBehaviour {
+
+    public Material matOrganic;
+    public Material matMechanic;
+
+    bool organic; // Right now a body part is either organic or mechanic. In the future a body part may be x % organic and (1 - x) % mechanic.
+
+    public bool Organic { get { return organic; } set { ;} }
+
+	// Use this for initialization
+	void Start () {
+        organic = true;
+        renderer.material = matOrganic;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
